515518ac64c3cee1b1bebd31d52bf0f374d0f4f1
rewerze <48671619+rewerze@users.noreply.github.com>
2024-11-18 10:11:51 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -15,24 +15,24 @@ sr.Close();
 Console.WriteLine($"3. feladat: Dolgozók száma: {dolgozok.Count()} fő");
 
 var linqAVG = dolgozok.Average(d => d.Ber);
-Console.WriteLine($"4. feladat: A bérek átlaga: {Math.Round(linqAVG / 1000, 1)}E Ft");
+Console.WriteLine($"4. feladat: A bérek átlaga: {Math.Round(linqAVG / 1000, 1)} eFt");
 
 Console.Write("5. feladat: Kérem a részleg nevét: ");
 string reszlegNev = Console.ReadLine();
 
 
-Console.WriteLine("6. feladat: A legtöbbet dolgozó a megadott részlegen");
+Console.Write("6. feladat: ");
 var linqReszleg = dolgozok.FirstOrDefault(d => d.Hely == reszlegNev);
 if (linqReszleg != null)
 {
     var linqMax = dolgozok.OrderByDescending(d => d.Ber).Where(d => d.Hely == reszlegNev).First();
     var linqMaxi = dolgozok.IndexOf(linqMax);
     Console.WriteLine(
-        $"\tNév: {dolgozok[linqMaxi].Nev}" +
+        $"A legtöbbet dolgozó a megadott részlegen" +
+        $"\n\tNév: {dolgozok[linqMaxi].Nev}" +
         $"\n\tNem: {dolgozok[linqMaxi].Nem}" +
         $"\n\tBelépés: {dolgozok[linqMaxi].Belepes}" +
-        $"\n\tBér: {dolgozok[linqMaxi].Ber:000 000} Forint"
-        );
+        $"\n\tBér: {dolgozok[linqMaxi].Ber:000 000} Forint");
 }
 else
 {
