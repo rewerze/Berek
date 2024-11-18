d1f501f22f7b13e66aa9596c309e388e8b49aba0
rewerze <48671619+rewerze@users.noreply.github.com>
2024-11-18 09:25:10 +0100

    added files

---
+++ Dolgozok.cs
@@ -0,0 +1,21 @@
+﻿namespace Berek
+{
+    internal class Dolgozok
+    {
+        public string Nev { get; set; }
+        public string Nem { get; set; }
+        public string Hely { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Dolgozok(string sor)
+        {
+            var db = sor.Split(';');
+            Nev = db[0];
+            Nem = db[1];
+            Hely = db[2];
+            Belepes = int.Parse(db[3]);
+            Ber = int.Parse(db[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Berek;
+using System.Text;
+
+List<Dolgozok> dolgozok = [];
+StreamReader sr = new("..\\..\\..\\src\\berek2020.txt", Encoding.UTF8);
+
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    dolgozok.Add(new(sr.ReadLine()));
+}
+sr.Close();
+
+
+Console.WriteLine($"3. feladat: Dolgozók száma: {dolgozok.Count()} fő");
+
+var linqAVG = dolgozok.Average(d => d.Ber);
+Console.WriteLine($"4. feladat: A bérek átlaga: {Math.Round(linqAVG / 1000, 1)}E Ft");
+
+Console.WriteLine("5. feladat: Kérem a részleg nevét: ");
+string reszlegNev = Console.ReadLine();
+
+
+Console.WriteLine("6. feladat");
+
+
+Console.WriteLine("7.feladat: Statisztika");
+var linqStat = dolgozok.GroupBy(d => d.Hely);
+foreach (var grp in linqStat) Console.WriteLine($"\t{grp.Key,-15}\t-{grp.Count(),4} fő");
+
+
+
+
+
